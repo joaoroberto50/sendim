d6182ada375a841893535ef0d7678d4694a76e56
joaoroberto50 <38048402+joaoroberto50@users.noreply.github.com>
2019-05-16 23:48:56 -0300

    Add files via upload

--- send.py
+++ send.py
@@ -2,11 +2,12 @@ import xlrd
 import pandas as pd
 import getpass
 import smtplib
+import os
+import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-from unidecode import unidecode
 
 def send(d_local, assunto, corpo, user_name, contato, anexo):
 	msg = MIMEMultipart()
@@ -39,15 +40,21 @@ def send(d_local, assunto, corpo, user_name, contato, anexo):
 			except Exception as error:
 				print("ERRO: ", error)
 
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= %s" % anexo)
+	try:
+		part = MIMEBase('application', 'octet-stream')
+		part.set_payload((attachment).read())
+		encoders.encode_base64(part)
+		part.add_header('Content-Disposition', "attachment; filename= %s" % anexo)
+	except Exception as wrong:
+		print(wrong)
 
-	msg.attach(part)
-	text = msg.as_string()
-	s.sendmail(user_name, contato, text)
-	print("E-mail enviado com sucesso para ", contato)
+	try:
+		msg.attach(part)
+		text = msg.as_string()
+		s.sendmail(user_name, contato, text)
+		print("E-mail enviado com sucesso para ", contato)
+	except Exception as wrong:
+		print(wrong)
 
 
 
@@ -59,7 +66,11 @@ def runer(nr, p_local, d_local, assunto, corpo, user_name):
 			send(d_local, assunto, corpo, user_name, contato, anexo)
 		else:
 			idnt = planilha['Nome'][i]
-			print(" nao possui email", idnt)
+			print("{} nao possui email".format(idnt))
+
+print("----------- BEM VINDO -----------")
+time.sleep(2)
+os.system('cls')
 
 
 logon = False
@@ -73,17 +84,38 @@ while(logon == False):
 		s.login(user_name, user_pass)
 		logon = True
 	except Exception as error:
+		os.system('cls')
 		print("ERRO: ", error)
 		logon = False
 
-assunto = input("insira o assunto do E-mail: ")
-l_txt = input("Insira o local do texto do E-mail com seu formato\nEx.:/home/joao/Downloads/planilha.txt: ")
+datas = False
+while(datas == False):
+	os.system('cls')
+	print("LOGIN efetuado com sucesso")
+	print('-'*30)
+	assunto = input("insira o assunto do E-mail: ")
+	l_txt = input("Insira o local do texto do E-mail com seu formato\nEx.:/home/joao/Downloads/planilha.txt: ")
+	p_local = input("Insira o local e o nome da tabela com seu formato\nEx.:/home/joao/Downloads/planilha.xls: ")
+	d_local = input("Insira o diretorio onde estao os documentos para anexo\nUsar o char barra invertida no final\nEx.:/home/joao/Downloads/: ")
+	print('-'*30)
+	tst = False
+	while(tst == False):
+		qst = input("\nEsta certo desses dados?\n1)Se esta digite 's'\n2)Digite 'm' para modificalos\n\t\t\t  >>> ")
+		if(qst == 's'):
+			datas = True
+			tst = True
+		elif(qst == 'm'):
+			datas = False
+			tst = True
+		else:
+			os.system('cls')
+			print("Entrada invalida - Digite novamente")
+
 texto = open(l_txt, 'r')
 corpo = texto.read()
-#texto.close()
-p_local = input("Insira o local e o nome da tabela com seu formato\nEx.:/home/joao/Downloads/planilha.xls: ")
-d_local = input("Insira o diretorio onde estao os documentos para anexo\nEx.:/home/joao/Downloads/: ")
-d_local = d_local + '/'
+
+os.system('cls')
+print('-' * 30)
 
 try:
 	planilha = pd.read_excel(r'' + p_local)
@@ -94,5 +126,16 @@ except Exception as error:
 	print("ERRO: ", error)
 
 runer(nr, p_local, d_local, assunto, corpo, user_name)
+
+print('-' * 30)
+x = 0
+while(x!=1):
+	tst = input("Para sair entre um caracter qualquer: >>> ")
+	x = 1
+
+print("----------- Efetuando Logout -----------")
+time.sleep(3)
+os.system('cls')
+print("------------- SAINDO -------------")
 
 s.quit()
